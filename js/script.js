9996f81c732dcdd549337b01f52b1fdc11feefca
melouttit <meghan.louttit@nytimes.com>
2016-03-21 20:06:40 -0400

    change background color

--- js/script.js
+++ js/script.js
@@ -1,7 +1,46 @@
-function tipCalculate(){
+//STEP 1
+var submit = document.getElementById('submit');
+submit.onclick = tipCalculate;
+
+function tipCalculate(slider, bill){
   console.log("TIP CALCULATE");
-  //the function that will run 
-  //when the submit button is clicked
-  //as defined in the inline javascript
-  //on index.html
+
+  //STEP 2
+  var totalBill = document.getElementById('bill').value;
+  console.log(totalBill);
+  var tipPerc = getPercChecked();
+
+  //STEP 6
+  console.log(tipPerc);
+  console.log(!isNaN(totalBill));
+  if(!isNaN(totalBill)){
+    var tipAmount = Math.floor(totalBill * tipPerc * 100) / 100;
+    //STEP 7
+    var roundedTip = tipAmount.toFixed(2);
+    var tipBox = document.getElementById('tipamount');
+    //STEP 8
+    tipBox.innerHTML = "$"+roundedTip;
+    console.log(roundedTip);
+  } else {
+    //STEP 9
+    alert('Please enter numbers only');
+    resetCalc();
+  }
 }
+
+//STEP 3
+function getPercChecked(){
+  var percBtns = document.getElementsByClassName('tip-btn');
+
+  //STEP 4
+  for(var i=0; i < percBtns.length; i++){
+    if(percBtns[i].checked){
+      //STEP 5
+      console.log(percBtns[i]);
+      return percBtns[i].value;
+    }
+  }
+}
+
+//FOR HOMEWORK
+function resetCalc(){}
